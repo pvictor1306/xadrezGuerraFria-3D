bc6230d15b10816bba545e086328fe0179c42d03
Paulo Victor <paulovictor.joao@gmail.com>
2016-11-15 23:30:37 -0200

    Incluindo o rei preto

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,14 @@ using Xadrez;
 class GameController : MonoBehaviour {
 
     public GameObject reiBranco = null;
+    public GameObject reiPreto = null;
 
     PartidaDeXadrez partida;
 
 	void Start () {
         partida = new PartidaDeXadrez();
         Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
+        Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto);
 	}
 	
 }
