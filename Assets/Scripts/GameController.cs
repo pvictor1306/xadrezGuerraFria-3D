db2f073c687e9dafe6bea05afdc946c051745bfc
Paulo Victor <paulovictor.joao@gmail.com>
2016-11-16 20:12:42 -0200

    Acrescentando as demais peças

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,16 @@ class GameController : MonoBehaviour {
     public GameObject reiPreto = null;
     public GameObject torreBranca = null;
     public GameObject torrePreta = null;
+    public GameObject damaPreta = null;
+    public GameObject damaBranca = null;
+    public GameObject cavaloBranco = null;
+    public GameObject cavaloPreto = null;
+    public GameObject bispoBranco = null;
+    public GameObject bispoPreto = null;
+    public GameObject peaoBranco = null;
+    public GameObject peaoPreto = null;
+
+
 
     public Text txtMsg = null;
     public Text txtXeque = null;
@@ -38,14 +48,39 @@ class GameController : MonoBehaviour {
         informarAguardando();
 
         Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
-        Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto);
+        Util.instanciarBispo('c', 1, Cor.Branca, partida, bispoBranco);
+        Util.instanciarCavalo('b', 1, Cor.Branca, partida, cavaloBranco);
+        Util.instanciarDama('d', 1, Cor.Branca, partida, damaBranca);
+        Util.instanciarBispo('f', 1, Cor.Branca, partida, bispoBranco);
+        Util.instanciarCavalo('g', 1, Cor.Branca, partida, cavaloBranco);
         Util.instanciarTorre('a', 1, Cor.Branca, partida, torreBranca);
         Util.instanciarTorre('h', 1, Cor.Branca, partida, torreBranca);
+        Util.instanciarPeao('a', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('b', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('c', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('d', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('e', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('f', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('g', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('h', 2, Cor.Branca, partida, peaoBranco);
+
+
         Util.instanciarTorre('a', 8, Cor.Preta, partida, torrePreta);
         Util.instanciarTorre('h', 8, Cor.Preta, partida, torrePreta);
-
-
-
+        Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto);
+        Util.instanciarBispo('c', 8, Cor.Preta, partida, bispoPreto);
+        Util.instanciarCavalo('b', 8, Cor.Preta, partida, cavaloPreto);
+        Util.instanciarDama('d', 8, Cor.Preta, partida, damaPreta);
+        Util.instanciarBispo('f', 8, Cor.Preta, partida, bispoPreto);
+        Util.instanciarCavalo('g', 8, Cor.Preta, partida, cavaloPreto);
+        Util.instanciarPeao('a', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('b', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('c', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('d', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('e', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('f', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('g', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('h', 7, Cor.Preta, partida, peaoPreto);
 
     }
 
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -22,6 +22,42 @@ class Util : MonoBehaviour {
 
     }
 
+    public static void instanciarDama(char coluna, int linha, Cor cor, PartidaDeXadrez partida, GameObject prefab) {
+        Vector3 pos = posicaoNaCena(coluna, linha);
+        GameObject dama = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+        Peca peca = new Dama(partida.tab, cor, dama);
+        partida.colocarNovaPeca(coluna, linha, peca);
+        dama.GetComponent<ReferenciaPeca>().peca = peca;
+
+    }
+
+    public static void instanciarBispo(char coluna, int linha, Cor cor, PartidaDeXadrez partida, GameObject prefab) {
+        Vector3 pos = posicaoNaCena(coluna, linha);
+        GameObject bispo = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+        Peca peca = new Bispo(partida.tab, cor, bispo);
+        partida.colocarNovaPeca(coluna, linha, peca);
+        bispo.GetComponent<ReferenciaPeca>().peca = peca;
+
+    }
+
+    public static void instanciarCavalo(char coluna, int linha, Cor cor, PartidaDeXadrez partida, GameObject prefab) {
+        Vector3 pos = posicaoNaCena(coluna, linha);
+        GameObject cavalo = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+        Peca peca = new Cavalo(partida.tab, cor, cavalo);
+        partida.colocarNovaPeca(coluna, linha, peca);
+        cavalo.GetComponent<ReferenciaPeca>().peca = peca;
+
+    }
+
+    public static void instanciarPeao(char coluna, int linha, Cor cor, PartidaDeXadrez partida, GameObject prefab) {
+        Vector3 pos = posicaoNaCena(coluna, linha);
+        GameObject peao = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+        Peca peca = new Peao(partida.tab, cor, partida, peao);
+        partida.colocarNovaPeca(coluna, linha, peca);
+        peao.GetComponent<ReferenciaPeca>().peca = peca;
+
+    }
+
     public static Vector3 posicaoNaCena(char coluna, int linha) {
         Vector3 posChao = GameObject.Find("planoDasPecas").transform.position;
         Vector3 posCasa = GameObject.Find("" + coluna + linha).transform.position;
