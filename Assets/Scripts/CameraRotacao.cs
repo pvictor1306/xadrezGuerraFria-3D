f5f3af3ac3bb6007c10966283149cc9591e75a39
Fritask <fritas.series@gmail.com>
2016-12-05 10:33:21 -0200

    Melhoria: girando a camera

---
+++ Assets/Scripts/CameraRotacao.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CameraRotacao : MonoBehaviour {
+
+    public float velocidadeAngular = 180f;
+
+    Vector3 posBranca, posPreta;
+    Quaternion rotBranca, rotPreta;
+
+    bool indoParaBranca, indoParaPreta;
+
+	void Start () {
+        indoParaBranca = false;
+        indoParaPreta = false;
+
+        posBranca = transform.position;
+        posPreta = posBranca;
+        posPreta.z = -posPreta.z;
+
+        Vector3 angulos = transform.rotation.eulerAngles;
+        rotBranca = Quaternion.Euler(angulos.x, angulos.y, angulos.z);
+        rotPreta = Quaternion.Euler(angulos.x, angulos.y + 180f, angulos.z);
+    }
+
+	
+	void Update () {
+	if (indoParaBranca) {
+            if (Vector3.Distance(transform.position, posBranca) > 0.5f) {
+                transform.RotateAround(Vector3.zero, Vector3.up, velocidadeAngular * Time.deltaTime);
+            }
+            else {
+                transform.position = posBranca;
+                transform.rotation = rotBranca;
+                indoParaBranca = false;
+            }
+        }
+    if (indoParaPreta) {
+            if (Vector3.Distance(transform.position, posPreta) > 0.5f) {
+                transform.RotateAround(Vector3.zero, Vector3.up, velocidadeAngular * Time.deltaTime);
+            }
+            else {
+                transform.position = posPreta;
+                transform.rotation = rotPreta;
+                indoParaPreta = false;
+            }
+        }
+    }
+
+    public void irParaBranca() {
+        indoParaBranca = true;
+    }
+    
+    public void irParaPreta() {
+        indoParaPreta = true;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -138,6 +138,7 @@ class GameController : MonoBehaviour {
                         else {
                             estado = Estado.AguardandoJogada;
                             informarAguardando();
+                            Invoke("girarCamera", 0.5f);
                             txtXeque.text = (partida.xeque) ? "XEQUE" : "";
                         }
                     }
@@ -197,7 +198,14 @@ class GameController : MonoBehaviour {
             GameObject dama = Instantiate(prefab, posPromovida, Quaternion.identity) as GameObject;
             pecaMovida.obj = dama;
         }
-
     }
-
+    
+    void girarCamera() {
+        if (partida.jogadorAtual == Cor.Branca) {
+            Camera.main.GetComponent<CameraRotacao>().irParaBranca();
+        }
+        else {
+            Camera.main.GetComponent<CameraRotacao>().irParaPreta();
+        }
+    }
 }
